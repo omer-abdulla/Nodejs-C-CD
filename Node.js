1626db38d1916a4f40e05f2b1a3c6dfa70a65c78
OMER ABDULLAH <omerabdullah555k@gmail.com>
2023-12-16 20:26:12 +0530

    node project

--- Node.js
+++ Node.js
@@ -1,2 +1,12 @@
-// hello.js
-console.log("Omer Abdullah Software Developer!")
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Muaadh Pharmacist RELA HOSPITAL\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
